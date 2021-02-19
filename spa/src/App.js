373150b781541c7e0113c22a0a0acb3e780177ef
Gabriel Cilico <gabrielcilico@gmail.com>
2021-02-19 19:25:06 -0300

    Ajustes finos de template

--- spa/src/App.js
+++ spa/src/App.js
@@ -6,6 +6,10 @@ function App() {
     <div className="App">
       <Hello />
       <Contador valorMinimo={10} valorMaximo={15}></Contador>
+      <Contador valorMinimo={20} valorMaximo={35}></Contador>
+      <Contador valorMinimo={45} valorMaximo={50}></Contador>
+      <Contador valorMinimo={50} valorMaximo={100}></Contador>
+      <Hello />
     </div>
   );
 }
